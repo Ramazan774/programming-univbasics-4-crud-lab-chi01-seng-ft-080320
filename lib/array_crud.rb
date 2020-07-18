dd0690c90847a1a8c6ecc5c4daba3305a621772a
Ramazan Ramazanov <ramazan.ramazanov@glion.ch>
2020-07-18 05:41:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   cars = ["BMW", "Mercedes", "Volvo", "Toyota"]
-  cars << "Lexus"
+  cars.push ("Lexus")
 end
 
 def add_element_to_start_of_array(array, element)
